1180c0b87912706279ff3be3db06986b94913eb0
awfixer <git@awfixer.me>
2025-10-20 14:25:09 -0700

    create README.md

--- modules/nixos/environment/default.nix
+++ modules/nixos/environment/default.nix
@@ -5,28 +5,20 @@
       gh
       glab
       ghunt
-      wireshark-cli
-      ruby
-      rails-new
-      tor-browser
       git-repo
       reposurgeon
       repomix
       cachix
       ghostty
-      obsidian
       autorandr
       youtube-music
       zed-editor
       devbox
       vesktop
-      llvm
-      llvmPackages.llvm
-      llvmPackages.llvm-manpages
-      clang
-      clang-tools
       yara
       brave
+      nixd
+      nil
     ];
   };
 }
